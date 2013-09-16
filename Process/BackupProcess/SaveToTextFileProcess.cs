5843b16b5ef79d03502ec382af8deeea814f1e5b
unknown <t-twu@MININT-O45UU34.fareast.corp.microsoft.com>
2013-09-16 16:46:03 +0800

    add TextItem Feature

--- Process/BackupProcess/SaveToTextFileProcess.cs
+++ Process/BackupProcess/SaveToTextFileProcess.cs
@@ -1,4 +1,5 @@
-﻿using LogReporter.Rule;
+﻿using LogReporter.Process.BackupProcess;
+using LogReporter.Rule;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,20 +13,19 @@ namespace LogReporter.Process
     public class SaveToTextFileProcess : BaseReportProcess
     {
         public const string TAG_RULE = "Rule";
+        public const string TAG_CONTENT = "Content";
+        public const string TAG_ITEM = "Item";
         public const string TAG_PATH = "Path";
         public const string TAG_FILE_NAME = "FileName";
-        public const string TAG_TEMPLETE = "Templete";
-        public const string TAG_GROUP = "ShowGroup";
 
         public string RuleName { get; set; }
-        public bool ShowGroup { get; set; }
         public string Path { get; set; }
         public string FileName { get; set; }
-        public string Templete { get; set; }
+        public List<TextItem> TextItems { get; set; }
 
         public SaveToTextFileProcess(XmlNode config)
         {
-            ShowGroup = false;
+            TextItems = new List<TextItem>();
             foreach (XmlNode node in config.ChildNodes)
             {
                 switch (node.Name)
@@ -39,19 +39,11 @@ namespace LogReporter.Process
                     case TAG_FILE_NAME:
                         CreateFileName(node);
                         break;
-                    case TAG_TEMPLETE:
-                        CreateTemplete(node);
-                        break;
-                    case TAG_GROUP:
-                        CreateShowGroup(node);
+                    case TAG_CONTENT:
+                        CreateContent(node);
                         break;
                 }
             }
-        }
-
-        private void CreateShowGroup(XmlNode node)
-        {
-            ShowGroup = bool.Parse(node.InnerText);
         }
 
         private void CreateRuleName(XmlNode node)
@@ -69,81 +61,25 @@ namespace LogReporter.Process
             FileName = node.InnerText;
         }
 
-        private void CreateTemplete(XmlNode node)
+        private void CreateContent(XmlNode config)
         {
-            Templete = node.InnerText;
+            foreach (XmlNode node in config.ChildNodes)
+            {
+                if (TAG_ITEM.Equals(node.Name))
+                {
+                    TextItem newItem = ItemFactory.CreateTextItem(node);
+                    if (newItem != null) TextItems.Add(newItem);
+                }
+            }
         }
 
         public override bool Process(Dictionary<string, BaseRule> rules)
         {
-            BaseRule inputRule = rules[RuleName];
-            if (inputRule.Count < 1) return false;
-
             StringBuilder newLineBuilder = new StringBuilder();
-            if (Templete == null)
+
+            foreach (TextItem item in TextItems)
             {
-                if (ShowGroup)
-                {
-                    Templete = "$$Name$$, $$Count$$";
-                }
-                else
-                {
-                    LogItem first = inputRule.LogItemsInRule[0];
-                    List<string> titles = new List<string>();
-
-                    foreach (var title in first.FieldNames)
-                    {
-                        titles.Add(title);
-                        newLineBuilder.Append("$$" + title + "$$,");
-                    }
-
-                    Templete = newLineBuilder.ToString();
-                }
-            }
-
-            newLineBuilder = new StringBuilder();
-            if (inputRule is GroupRule)
-            {
-                foreach (var rule in (inputRule as GroupRule).Rules)
-                {
-                    if (ShowGroup)
-                    {
-                        LogItem groupItem = new LogItem();
-                        groupItem.SetField("Name", rule.RuleName);
-                        groupItem.SetField("Count", rule.Count);
-
-                        string content = Templete;
-                        content = content.Replace("$$Name$$", groupItem.TryGetField("Name", "").ToString());
-                        content = content.Replace("$$Count$$", groupItem.TryGetField("Count", "").ToString());
-                        newLineBuilder.AppendLine(content);
-                    }
-                    else
-                    {
-                        foreach (var item in rule.LogItemsInRule)
-                        {
-                            string content = Templete;
-                            foreach (string title in item.FieldNames)
-                            {
-                                content = content.Replace("$$" + title + "$$", item.TryGetField(title, "").ToString());
-                            }
-
-                            newLineBuilder.AppendLine(content);
-                        }
-                    }
-                }
-            }
-            else
-            {
-                foreach (var item in inputRule.LogItemsInRule)
-                {
-                    string content = Templete;
-                    foreach (string title in item.FieldNames)
-                    {
-                        content = content.Replace("$$" + title + "$$", item.TryGetField(title, "").ToString());
-                    }
-
-                    newLineBuilder.AppendLine(content);
-                }
+                newLineBuilder.AppendLine(item.GetContent(rules));
             }
 
             string fileName = Path + FileName + DateTime.Now.ToFileTime() + ".txt";
---
+++ Process/BackupProcess/TextItem.cs
@@ -0,0 +1,141 @@
+﻿using LogReporter.Rule;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LogReporter.Process.BackupProcess
+{
+    public class TextItemConstant
+    {
+        public const string TYPE_TEXT = "text";
+        public const string TYPE_LIST = "list";
+
+        public const string ATT_TYPE = "type";
+        public const string ATT_RULE = "rule";
+        public const string ATT_ORDERBY = "orderby";
+        public const string ATT_DESC = "desc";
+
+        public const string FIELD_NAME = "Name";
+        public const string FIELD_COUNT = "Count";
+    }
+
+    public class ItemFactory
+    {
+        public static TextItem CreateTextItem(XmlNode config)
+        {
+            string type = config.Attributes[TextItemConstant.ATT_TYPE].Value;
+
+            if (TextItemConstant.TYPE_LIST.Equals(type))
+                return new ListItem(config);
+
+            return new TextItem(config);
+        }
+    }
+
+    public class TextItem
+    {
+        public string Content { get; set; }
+        public string Type { get; set; }
+
+        public TextItem(XmlNode config)
+        {
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tx = new XmlTextWriter(sw);
+            tx.Formatting = Formatting.Indented;
+
+            config.WriteContentTo(tx);
+
+            Content = sw.ToString();
+            Type = TextItemConstant.ATT_TYPE;
+        }
+
+        public virtual string GetContent(Dictionary<string, BaseRule> rules)
+        {
+            return Content;
+        }
+    }
+
+    public class ListItem : TextItem
+    {
+        public string RuleName { get; set; }
+        public string OrderBy { get; set; }
+        public bool Desc { get; set; }
+
+        public ListItem(XmlNode config)
+            : base(config)
+        {
+            Type = TextItemConstant.TYPE_LIST;
+            OrderBy = null;
+            Desc = false;
+
+            RuleName = config.Attributes[TextItemConstant.ATT_RULE].Value;
+
+            foreach (XmlAttribute att in config.Attributes)
+            {
+                switch (att.Name)
+                {
+                    case TextItemConstant.ATT_ORDERBY:
+                        OrderBy = att.Value;
+                        break;
+                    case TextItemConstant.ATT_DESC:
+                        Desc = bool.Parse(att.Value);
+                        break;
+                }
+            }
+        }
+
+        public override string GetContent(Dictionary<string, BaseRule> rules)
+        {
+            IEnumerable<LogItem> logItems = Sort(GetLogItem(rules[RuleName]));
+
+            StringBuilder newLineBuilder = new StringBuilder();
+            foreach (var item in logItems)
+            {
+                List<string> fields = item.FieldNames;
+                string newLine = Content;
+                foreach (var name in fields)
+                {
+                    newLine = newLine.Replace("$$" + name + "$$", item.TryGetField(name, "").ToString());
+                }
+
+                newLineBuilder.AppendLine(newLine);
+            }
+
+            return newLineBuilder.ToString();
+        }
+
+        private IEnumerable<LogItem> Sort(List<LogItem> items)
+        {
+            var outcome = from s in items select s;
+            if (OrderBy != null)
+                outcome = outcome.OrderBy(item => item.TryGetField(OrderBy, ""));
+            if (Desc)
+                outcome = outcome.Reverse();
+
+            return outcome;
+        }
+
+        private List<LogItem> GetLogItem(BaseRule rule)
+        {
+            if (rule is GroupRule)
+            {
+                List<LogItem> outcome = new List<LogItem>();
+                foreach (BaseRule subRule in (rule as GroupRule).Rules)
+                {
+                    LogItem item = new LogItem();
+                    item.SetField(TextItemConstant.FIELD_NAME, subRule.RuleName);
+                    item.SetField(TextItemConstant.FIELD_COUNT, subRule.Count);
+                    outcome.Add(item);
+                }
+
+                return outcome;
+            }
+
+            return rule.LogItemsInRule;
+        }
+    }
+}
